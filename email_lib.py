0170615a08cfae2a4a8668bc81ded4958d56261a
midnightslacker <midnightslacker11@gmail.com>
2014-11-20 14:25:36 -0500

    Added support for decoding utf-8

--- email_lib.py
+++ email_lib.py
@@ -3,12 +3,11 @@ import email
 import html2text
 import ConfigParser
 import os
-import re
 from pprint import pprint  
 
-list_response_pattern = re.compile(r'\((?P<flags>.*?)\) "(?P<delimiter>.*)" (?P<name>.*)')
-
 def open_connection(verbose=False):
+    ''' Establish imap SSL connection to inbox '''
+
     # Read the config file
     config = ConfigParser.ConfigParser()
     config.read([os.path.expanduser('~/dev/email/.mail_config.ini')])
@@ -21,35 +20,43 @@ def open_connection(verbose=False):
     # Login to account
     username = config.get('Account', 'username')
     password = config.get('Account', 'password')
+    
     if verbose: print 'Logging in as', username
     connection.login(username, password)
     return connection
 
-def parse_response(resp):
-    # parse output for given server response and return each entity
-    flags, delimiter, mailbox_name = list_response_pattern.match(resp).groups()
-    mailbox_name = mailbox_name.strip('"')
-    return(flags, delimiter, mailbox_name)
-
 def list_mailboxes(open_connect):
-    # print out list of available mailboxes
+    ''' Print out list of available mailboxes '''
+    
     typ, data = open_connect.list()
     pprint(data)
 
 def list_mailboxes_by_pattern(open_connect, pattern):
-    # list folders matching a pattern
+    ''' List folders matching a pattern '''
+    
     typ, data = open_connect.list(pattern=pattern)
     for line in data:
         print("Server Response: ", line)
 
 def list_subfolders(open_connect, inbox):
-    # print the list of subfolders in a given inbox
+    ''' Print the list of subfolders in a given inbox '''
+    
     typ, data = open_connect.list(directory=inbox)
     for line in data:
         print("Server Response: ", line)
 
+def list_email_parts(open_connect, inbox, msg_id):
+    ''' Display different parts of the email '''
+    open_connect.select(inbox, readonly=True)
+    result,data = open_connect.fetch(msg_id, '(RFC822)')
+    raw_email = data[0][1]
+    email_message = email.message_from_string(raw_email)
+    for part in email_message.items():
+        print part
+
 def get_status(open_connect, inbox=""):
-    # get status for all or a given folder
+    '''Get status for all or a given folder '''
+    
     if inbox == "":
         typ, data = open_connect.list()
     else:
@@ -60,13 +67,40 @@ def get_status(open_connect, inbox=""):
         print (open_connect.status(mailbox_name, '(MESSAGES RECENT UIDNEXT UIDVALIDITY UNSEEN)') )
 
 def get_numMessages(open_connect, inbox):
-    # return the number of messages in a given inbox
+    ''' return the number of messages in a given inbox'''
+
     typ, data = open_connect.select(inbox)
     num_msgs = int(data[0])
     return num_msgs
 
+def get_date(open_connect, inbox, msg_id):
+    ''' grab the date field of the email header'''
+    
+    open_connect.select(inbox, readonly=True)
+    typ, msg_data = open_connect.fetch(msg_id, '(RFC822)')
+    for response in msg_data:
+        if isinstance(response, tuple):
+            msg = email.message_from_string(response[1])
+            for header in ['date']:
+                date_header = msg[header]
+    
+    return date_header
+
+def get_subject(open_connect, inbox, msg_id):
+    ''' grab the subject field of the email header '''
+    
+    typ, msg_data = open_connect.fetch(msg_id, '(RFC822)')
+    for response in msg_data:
+        if isinstance(response, tuple):
+            msg = email.message_from_string(response[1])
+            for header in ['subject']:
+                subject_header = msg[header]
+
+    return subject_header
+
 def get_email_header(open_connect, inbox, msg_id):
-    # grab email header
+    ''' If you just want the header '''
+
     email_header = ''
     open_connect.select(inbox, readonly=True)
     typ, msg_data = open_connect.fetch(str(msg_id), '(RFC822)')
@@ -77,19 +111,64 @@ def get_email_header(open_connect, inbox, msg_id):
                 email_header += '%-8s: %s' %(header.upper(), msg[header]) + '\n'
     return  email_header
 
-def get_email_body(open_connect, inbox, msg_id):
-    # grab email
-    email_message = ''
+def get_decoded_email_body(message_body):
+    """ Decode utf-8 encoding """
+    
+    msg = email.message_from_string(message_body)
+    text = ""
+
+    if msg.is_multipart():
+        html = None
+        
+        for part in msg.get_payload():
+            charset = part.get_content_charset()
+ 
+            if part.get_content_type() == 'text/plain':
+                text = unicode(part.get_payload(decode=True), str(charset), "ignore").encode('utf8', 'replace')
+ 
+            if part.get_content_type() == 'text/html':
+                html = unicode(part.get_payload(decode=True), str(charset), "ignore").encode('utf8', 'replace')
+ 
+        if text is not None:
+            return text
+        else:
+            return html2text.html2text(html)
+    else:
+        text = unicode(msg.get_payload(decode=True), msg.get_content_charset(), 'ignore').encode('utf8', 'replace')
+        return text
+
+def get_email_by_msgID(open_connect, inbox, msg_id):
+    ''' Get email by msgID '''
+    email_header=''
     open_connect.select(inbox, readonly=True)
-    typ, msg_data = open_connect.fetch(str(msg_id), '(RFC822)')
-    for response in msg_data:
-        if isinstance(response, tuple):
-            msg = email.message_from_string(response[1])
-            for line in email.iterators.body_line_iterator(msg):
-                email_string = html2text.html2text(line)
-                email_message += email_string
-    return (email_message)
+    result,data = open_connect.fetch(msg_id, '(RFC822)')
+    raw_email = data[0][1]
+    email_message = email.message_from_string(raw_email)
+    
+    # Grab the email header info
+    for header in [ 'to', 'from', 'subject', 'date' ]:
+        email_header += '%-8s: %s' %(header.upper(), email_message[header]) + '\n'
 
+    # Now get body
+    for part in email_message.get_payload():
+        charset = email_message.get_content_charset()
+        
+        # check for utf-8 encoding
+        if str(charset) == "utf-8":
+            email_body = get_decoded_email_body(raw_email)
+            return str(email_header) + str(email_body)
 
+    # Check to see if email is multipart (has multiple parts)
+    email_body = ''
+    
+    if email_message.is_multipart():
+        for part in email_message.get_payload():
+            if part.get_content_type() == "text/plain":
+                email_body = part.get_payload()
+            elif part.get_content_type() == "text/html":
+                email_body = part.get_payload()
+    else:
+        email_body = email_message.get_payload()
 
+    return str(email_header) + (email_body)
 
